7df47da83218a2d7f77e6a25b165dee3f9a163ce
Arpit Pandey <52160632+Arpitpandey99@users.noreply.github.com>
2021-06-25 21:39:12 +0530

    Update cnn_my.py

--- cnn_my.py
+++ cnn_my.py
@@ -59,5 +59,17 @@ classifier.fit_generator(training_set,
                          validation_data = test_set,
                          validation_steps = (2000/32))
 
-
+# Predicting result 
+from keras.preprocessing import image
+import numpy as np
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg',
+                            target_size = (64,64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image,axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
 
